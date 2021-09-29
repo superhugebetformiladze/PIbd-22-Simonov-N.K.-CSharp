a678f8db06d06c6fb41da69cf980a20e998a0bf1
Иванов Иван <BROTON07@yandex.ru>
2021-09-29 20:14:21 +0400

    hi

--- lab1_TP_v2/Program.cs
+++ lab1_TP_v2/Program.cs
@@ -14,6 +14,7 @@ namespace lab1_TP_v2
         [STAThread]
         static void Main()
         {
+        //11
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
